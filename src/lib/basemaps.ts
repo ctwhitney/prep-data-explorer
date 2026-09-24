f7cbf1142038582644dc99abd27a346bf6823aa1
Chris Whitney <chris.whitney@unh.edu>
2026-09-24 15:31:01 -0400

    Fix CARTO basemap authentication

--- src/lib/basemaps.ts
+++ src/lib/basemaps.ts
@@ -2,7 +2,7 @@ export default [
   {
     name: 'Carto',
     visible: true,
-    url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+    url: `https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png?key=${import.meta.env.VITE_CARTO_API_KEY}`,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     options: {
       subdomains: 'abcd',
